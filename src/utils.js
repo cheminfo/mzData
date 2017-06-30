6ad6441dd4467fba90572509a6febdb3e2368078
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-06-30 16:21:30 +0200

    chore(utils): use typed array in the response

--- src/utils.js
+++ src/utils.js
@@ -2,17 +2,15 @@ import {toByteArray} from 'base64-js';
 
 export function decoder(encoded, options) {
     var buffer = toByteArray(encoded);
-    var float;
     if (options.precision === '64') {
-        float = new Float64Array(buffer.buffer);
+        return new Float64Array(buffer.buffer);
     } else {
-        float = new Float32Array(buffer.buffer);
+        return new Float32Array(buffer.buffer);
     }
-    return Array.from(float);
 }
 
 export function mergeSeries(first, second) {
-    var ans = first.slice();
+    var ans = new Array(first.length);
     for (var i = 0; i < ans.length; i++) {
         ans[i] = [first[i], second[i]];
     }

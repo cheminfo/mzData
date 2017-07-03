e7e314b1b704da95732f526db96d9634fa4794e8
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-07-03 17:16:23 +0200

    test(mzData): add real test case

--- src/__tests__/mzData.js
+++ src/__tests__/mzData.js
@@ -1,6 +1,6 @@
 import {mzData} from '..';
 import {readFileSync} from 'fs';
-const pathFiles = __dirname + '/../../testFiles/';
+const pathFiles = __dirname + '/../../testFiles/mzData/';
 
 describe('mzData', () => {
     it('read tiny.mzData.xml', () => {
@@ -10,5 +10,15 @@ describe('mzData', () => {
         expect(response.series.ms.data.length).toBe(2);
         expect(response.series.ms.data[0][0].length).toBe(1313);
         expect(response.series.ms.data[1][0].length).toBe(43);
+    });
+
+    it('read real file', () => {
+        const data = readFileSync(pathFiles + '3-28-5.mzdata.xml');
+        var response = mzData(data);
+        expect(response.times.length).toBe(3029);
+        expect(response.series.ms.data.length).toBe(3029);
+        expect(response.series.ms.data[0][0].length).toBe(62);
+        expect(response.series.ms.data[1][0].length).toBe(1);
+        expect(response.series.ms.data[2][0].length).toBe(0);
     });
 });

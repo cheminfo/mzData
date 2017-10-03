dbcc2206de8bbd037629246d842769b4e4a9e754
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2017-10-02 20:55:30 -0500

    refactor: rename the hole project
    BREAKING CHANGE
    closes #3

--- README.md
+++ README.md
@@ -1,37 +1,37 @@
-# mzMLjs
+# mzData
 
   [![NPM version][npm-image]][npm-url]
   [![build status][travis-image]][travis-url]
   [![Test coverage][codecov-image]][codecov-url]
   [![npm download][download-image]][download-url]
 
-Read and explore mzML files.
+Read and explore mzData v1.05 files.
 
 ## Installation
 
-`$ npm install --save mzmjs`
+`$ npm install --save mzdata`
 
 ## Usage
 
 ```js
-import {mzData} from 'mzmjs';
+import mzData from 'mzdata';
 
 // mzData files
 const mzDataFile = readFileSync(__dirname + '/tiny.mzData.xml');
 var response = mzData(mzDataFile);
 ```
 
-## [API Documentation](https://cheminfo-js.github.io/mzMLjs/)
+## [API Documentation](https://cheminfo-js.github.io/mzData/)
 
 ## License
 
   [MIT](./LICENSE)
 
-[npm-image]: https://img.shields.io/npm/v/mzmjs.svg?style=flat-square
-[npm-url]: https://npmjs.org/package/mzmjs
-[travis-image]: https://img.shields.io/travis/cheminfo-js/mzMLjs/master.svg?style=flat-square
-[travis-url]: https://travis-ci.org/cheminfo-js/mzMLjs
-[codecov-image]: https://img.shields.io/codecov/c/github/cheminfo-js/mzMLjs.svg?style=flat-square
-[codecov-url]: https://codecov.io/github/cheminfo-js/mzMLjs
-[download-image]: https://img.shields.io/npm/dm/mzmjs.svg?style=flat-square
-[download-url]: https://npmjs.org/package/mzmjs
+[npm-image]: https://img.shields.io/npm/v/mzdata.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/mzdata
+[travis-image]: https://img.shields.io/travis/cheminfo-js/mzData/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/cheminfo-js/mzData
+[codecov-image]: https://img.shields.io/codecov/c/github/cheminfo-js/mzData.svg?style=flat-square
+[codecov-url]: https://codecov.io/github/cheminfo-js/mzData
+[download-image]: https://img.shields.io/npm/dm/mzdata.svg?style=flat-square
+[download-url]: https://npmjs.org/package/mzdata
--- src/index.js
+++ src/index.js
@@ -1,1 +1,79 @@
-export {mzData} from './mzData';
+import {parser} from 'sax';
+import {decoder, mergeSeries} from './utils';
+
+/**
+ * Reads a mzData v1.05 file
+ * @param {ArrayBuffer} data - ArrayBuffer or any Typed Array (including Node.js' Buffer from v4) with the data
+ * @return {{times: Array<number>, series: { ms: { data:Array<Array<number>>}}}}
+ */
+export default function mzData(data) {
+    const xml = parser(true, {trim: true});
+
+    var result = {
+        times: [],
+        series: {}
+    };
+    var error = [];
+    var binaryData = {};
+    var mz = [];
+    var int = [];
+    var kind;
+
+    xml.onopentag = (node) => {
+        // eslint-disable-next-line default-case
+        switch (node.name) {
+            case 'mzData':
+                kind = 'mzData';
+                break;
+            case 'cvParam':
+                if (node.attributes.name === 'TimeInMinutes') {
+                    result.times.push(Number(node.attributes.value));
+                }
+                break;
+            case 'mzArrayBinary':
+                binaryData = {
+                    serie: 'mz'
+                };
+                break;
+            case 'intenArrayBinary':
+                binaryData = {
+                    serie: 'int'
+                };
+                break;
+            case 'data':
+                if (node.isSelfClosing) {
+                    if (binaryData.serie === 'mz') {
+                        mz.push([]);
+                    } else if (binaryData.serie === 'int') {
+                        int.push([]);
+                    }
+                    binaryData = {};
+                } else if (binaryData.serie) {
+                    binaryData.precision = node.attributes.precision;
+                }
+                break;
+        }
+    };
+
+    xml.ontext = (raw) => {
+        if (binaryData.serie === 'mz') {
+            mz.push(decoder(raw, binaryData));
+        } else if (binaryData.serie === 'int') {
+            int.push(decoder(raw, binaryData));
+        }
+        binaryData = {};
+    };
+
+    xml.onerror = (err) => error.push(err);
+    xml.write(data).close();
+    if (!kind || kind !== 'mzData') {
+        throw new TypeError('Not a mzData file');
+    }
+    if (error.length > 0) {
+        throw new Error(error);
+    }
+
+    result.series.ms = {};
+    result.series.ms.data = mergeSeries(mz, int);
+    return result;
+}

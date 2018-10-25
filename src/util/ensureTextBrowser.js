16be6dba27d0a0816eb09d30858ca4d63f7562b4
Luc Patiny <luc@patiny.com>
2018-10-25 09:39:06 +0200

    convert ArrayBuffer in the browser

--- src/util/ensureTextBrowser.js
+++ src/util/ensureTextBrowser.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function ensureTextBrowser(data) {
-  if (ArrayBuffer.isView(data)) {
+  if (ArrayBuffer.isView(data) || data instanceof ArrayBuffer) {
     // eslint-disable-next-line no-undef
     return new TextDecoder('utf8').decode(data);
   }

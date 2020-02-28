c1199f887358c8a1e2e3936f14a0e708427c0fcd
Luc Patiny <luc@patiny.com>
2020-02-28 17:25:24 +0100

    fix: ensureText

--- src/util/ensureText.js
+++ src/util/ensureText.js
@@ -1,9 +1,9 @@
 export function ensureText(data, options = {}) {
   const { encoding = 'utf8' } = options;
-  if (Buffer && data instanceof Buffer) {
+  if (typeof Buffer !== 'undefined' && data instanceof Buffer) {
     return data.toString(encoding);
   }
-  if (ArrayBuffer && data instanceof ArrayBuffer) {
+  if (typeof ArrayBuffer !== 'undefined' && data instanceof ArrayBuffer) {
     return new TextDecoder(encoding).decode(data);
   }
   return data;

1a2102f45742d6a1b09e6ef8d53b8a9d5ce68fba
Luc Patiny <luc.patiny@epfl.ch>
2020-05-03 19:33:30 +0200

    fix: big endian in mzxml

--- src/mzxml/parseBinaryDataArray.js
+++ src/mzxml/parseBinaryDataArray.js
@@ -8,7 +8,8 @@ export function parseBinaryDataArray(node) {
   let attr = node._attr;
   let cvParam = parseCvParam(data._attr);
   if (!data || !attr) return [];
-  let buffer = decoder(data._data, cvParam);
+  let bytes = decoder(data._data, cvParam);
+
   let kind = '';
   if (cvParam.contentType) {
     kind = cvParam.contentType;
@@ -17,15 +18,20 @@ export function parseBinaryDataArray(node) {
   } else {
     throw new Error('unknown binary data type');
   }
+
   // console.log(buffer)
   if (cvParam.precision === 64) {
     let result = {};
-    result.data = new Float64Array(buffer.buffer);
+    let buffer = Buffer.from(bytes);
+    result.data = new Float64Array(buffer.length / 8);
+    for (let i = 0; i < buffer.length; i += 8) {
+      result.data[i / 8] = buffer.readDoubleBE(i);
+    }
     result.kind = kind;
     return result;
   } else if (cvParam.precision === 32) {
     let result = {};
-    result.data = new Float32Array(buffer);
+    result.data = new Float32Array(bytes);
     result.kind = kind;
     return result;
   }

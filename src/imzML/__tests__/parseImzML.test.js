0c549d5bbeee178ae263096c1ba83a4ba5977a4e
Luc Patiny <luc@patiny.com>
2025-11-23 15:43:07 +0100

    test: fix test for parseImzML

---
+++ src/imzML/__tests__/parseImzML.test.js
@@ -0,0 +1,25 @@
+import { openAsBlob, readFileSync } from 'fs';
+import { join } from 'path';
+
+import { expect, test } from 'vitest';
+
+import { parseMzML } from '../../mzml/parseMzML.js';
+
+const pathFiles = join(__dirname, 'data');
+
+test('Small test', async () => {
+  const blobIBD = await openAsBlob(join(pathFiles, 'test.ibd'));
+  const ibd = await blobIBD.arrayBuffer();
+
+  const blobIMZML = await openAsBlob(join(pathFiles, 'test.imzML'));
+  const imzML = await blobIMZML.arrayBuffer();
+
+  let response = await parseMzML(imzML, { rawData: ibd });
+
+  expect(response.times).toHaveLength(15496);
+
+  expect(response.series.ms.data).toHaveLength(15496);
+  expect(response.series.ms.data[0][0]).toHaveLength(475);
+  expect(response.series.ms.data[0][1]).toHaveLength(475);
+  expect(response.series.ms.data[0]).toMatchSnapshot();
+});
